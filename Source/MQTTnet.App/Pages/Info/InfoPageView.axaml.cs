33b3a6cadfde2791e2c0bf6f0aae629b6b93ee22
Christian <6939810+chkr1011@users.noreply.github.com>
2022-01-19 21:15:29 +0100

    Add Info page.

---
+++ Source/MQTTnet.App/Pages/Info/InfoPageView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MQTTnet.App.Pages.Info;
+
+public class InfoPageView : UserControl
+{
+    public InfoPageView()
+    {
+        InitializeComponent();
+        
+        
+    }
+
+    void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
---
+++ Source/MQTTnet.App/Pages/Info/InfoPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MQTTnet.App.Common;
+using MQTTnet.Client;
+using ReactiveUI;
+
+namespace MQTTnet.App.Pages.Info;
+
+public sealed class InfoPageViewModel : BasePageViewModel
+{
+    public InfoPageViewModel()
+    {
+        Header = "Info";
+
+        MqttNetAppVersion = typeof(App).Assembly.GetName().Version?.ToString() ?? "<unknown>";
+        MqttNetVersion = typeof(MqttClient).Assembly.GetName().Version?.ToString() ?? "<unknown>";
+        DotNetVersion = Environment.Version.ToString();
+    }
+    
+    public string MqttNetAppVersion { get; }
+    
+    public string MqttNetVersion { get; }
+    
+    public string DotNetVersion { get; }
+}
