3f45efa3cefd60fc3eaece51ab2a8ca256103e4e
Christian <6939810+chkr1011@users.noreply.github.com>
2022-02-10 20:42:05 +0100

    Add log.

---
+++ Source/Pages/Log/LogItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MQTTnetApp.Pages.Log;
+
+public sealed class LogItemViewModel
+{
+    public string Level { get; set; }
+    
+    public string Timestamp { get; set; }
+    
+    public string Source { get; set; }
+    
+    public string Message { get; set; }
+}
--- Source/Pages/Log/LogPageViewModel.cs
+++ Source/Pages/Log/LogPageViewModel.cs
@@ -1,7 +1,43 @@
-﻿using MQTTnetApp.Common;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia.Threading;
+using MQTTnet.Diagnostics;
+using MQTTnetApp.Common;
+using MQTTnetApp.Services.Mqtt;
 
 namespace MQTTnetApp.Pages.Log;
 
 public sealed class LogPageViewModel : BaseViewModel
 {
+    readonly MqttClientService? _mqttClientService;
+
+    public LogPageViewModel(MqttClientService mqttClientService)
+    {
+        _mqttClientService = mqttClientService ?? throw new ArgumentNullException(nameof(mqttClientService));
+
+        _mqttClientService.LogMessagePublished += MqttClientServiceOnLogMessagePublished;
+    }
+
+    public ObservableCollection<LogItemViewModel> Items { get; } = new();
+
+    public void ClearItems()
+    {
+        Items.Clear();
+    }
+    
+    void MqttClientServiceOnLogMessagePublished(MqttNetLogMessagePublishedEventArgs obj)
+    {
+        var newItem = new LogItemViewModel
+        {
+            Timestamp = obj.LogMessage.Timestamp.ToString("HH:mm:ss.fff"),
+            Level = obj.LogMessage.Level.ToString(),
+            Source = obj.LogMessage.Source,
+            Message = obj.LogMessage.Message
+        };
+        
+        Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            Items.Add(newItem);
+        });
+    }
 }
