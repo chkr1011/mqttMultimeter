a1ee713a561391746406f362a89f27ab49558353
Christian <6939810+chkr1011@users.noreply.github.com>
2022-02-12 17:36:20 +0100

    Create LogPageView.axaml.cs

---
+++ Source/Pages/Log/LogPageView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MQTTnetApp.Pages.Log;
+
+public class LogPageView : UserControl
+{
+    public LogPageView()
+    {
+        InitializeComponent();
+    }
+
+    void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
