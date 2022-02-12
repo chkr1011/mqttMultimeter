9368eb13cb5b19291a77729566b489416fcb0ffb
Christian <6939810+chkr1011@users.noreply.github.com>
2022-02-12 17:28:11 +0100

    Add log page (#23)

--- Source/Pages/Log/LogPageViewModel.cs
+++ Source/Pages/Log/LogPageViewModel.cs
@@ -1,7 +1,60 @@
-﻿using MQTTnetApp.Common;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia.Threading;
+using MQTTnet.Diagnostics;
+using MQTTnetApp.Common;
+using MQTTnetApp.Services.Mqtt;
+using ReactiveUI;
 
 namespace MQTTnetApp.Pages.Log;
 
 public sealed class LogPageViewModel : BaseViewModel
 {
+    bool _isRecordingEnabled;
+
+    public LogPageViewModel(MqttClientService mqttClientService)
+    {
+        var mqttClientService1 = mqttClientService ?? throw new ArgumentNullException(nameof(mqttClientService));
+
+        mqttClientService1.LogMessagePublished += MqttClientServiceOnLogMessagePublished;
+    }
+
+    public bool IsRecordingEnabled
+    {
+        get => _isRecordingEnabled;
+        set => this.RaiseAndSetIfChanged(ref _isRecordingEnabled, value);
+    }
+
+    public ObservableCollection<LogItemViewModel> Items { get; } = new();
+    
+    public void ClearItems()
+    {
+        Items.Clear();
+    }
+
+    void MqttClientServiceOnLogMessagePublished(MqttNetLogMessagePublishedEventArgs eventArgs)
+    {
+        if (!_isRecordingEnabled)
+        {
+            return;
+        }
+
+        var newItem = new LogItemViewModel
+        {
+            IsVerbose = eventArgs.LogMessage.Level == MqttNetLogLevel.Verbose,
+            IsInformation = eventArgs.LogMessage.Level == MqttNetLogLevel.Info,
+            IsWarning = eventArgs.LogMessage.Level == MqttNetLogLevel.Warning,
+            IsError = eventArgs.LogMessage.Level == MqttNetLogLevel.Error,
+            Timestamp = eventArgs.LogMessage.Timestamp.ToString("HH:mm:ss.fff"),
+            Source = eventArgs.LogMessage.Source,
+            Message = eventArgs.LogMessage.Message,
+            Level = eventArgs.LogMessage.Level.ToString(),
+            Exception = eventArgs.LogMessage.Exception?.ToString()
+        };
+
+        Dispatcher.UIThread.Post(() =>
+        {
+            Items.Add(newItem);
+        });
+    }
 }
