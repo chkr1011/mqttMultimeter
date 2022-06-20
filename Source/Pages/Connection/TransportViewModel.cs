edc44d3f23ff8af3e4ce32c48d8e2f30b990a0c8
Christian <6939810+chkr1011@users.noreply.github.com>
2022-06-20 20:59:34 +0200

    Fix designer.

--- Source/Pages/Connection/TransportViewModel.cs
+++ Source/Pages/Connection/TransportViewModel.cs
@@ -4,11 +4,11 @@ namespace MQTTnetApp.Pages.Connection;
 
 public sealed class TransportViewModel : EnumViewModel<Transport>
 {
-    public TransportViewModel(object displayValue, Transport value) : base(displayValue, value)
+    public TransportViewModel(object? displayValue, Transport value) : base(displayValue, value)
     {
     }
 
-    public object HostDisplayValue { get; set; }
+    public object? HostDisplayValue { get; set; }
 
     public bool IsPortAvailable { get; set; }
 }
